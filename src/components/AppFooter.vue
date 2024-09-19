f54fd18bba050c1b45b1754b4d8bae6bd283edcd
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-09-19 15:09:50 +0200

    improved layout

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,33 @@
+<script>
+    export default {
+        name: 'AppFooter'
+    };
+</script>
+
+<template>
+    <footer class="footer">
+        <div class="container d-flex justify-content-between py-3 text-white">
+            <p>&copy; 2024 Yu-Gi-Oh Boolean App.</p>
+            <div>
+                <span>Created by </span><a href="https://github.com/Francesco-Matteucci">Francesco Matteucci</a>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+    .footer {
+        background-color: #333;
+        color: white;
+        width: 100%;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        height: 60px;
+
+        a {
+
+            color: #fff;
+        }
+    }
+</style>
--- src/components/ArchetypeSelect.vue
+++ src/components/ArchetypeSelect.vue
@@ -30,32 +30,60 @@
             onSelectArchetype() {
                 // Emetto l'event che passa l'archetipo selezionato al componente genitore
                 this.$emit('archetype-selected', this.selectedArchetype);
+            },
+            refreshPage() {
+                window.location.reload();
             }
         }
     };
 </script>
 
 <template>
-    <div class="mb-4">
-        <label for="archetypeSelect" class="form-label text-white">Select Archetype</label>
+    <div class="mb-4 fw-bold d-flex">
         <select id="archetypeSelect" class="form-select" v-model="selectedArchetype" @change="onSelectArchetype">
             <option value="">Select your archetype</option>
             <option v-for="archetype in archetypes" :key="archetype.archetype_name" :value="archetype.archetype_name">
                 {{ archetype.archetype_name }}
             </option>
         </select>
+        <img class="ms-4" src="../assets/icon.png" alt="Yu-Gi-Oh Card" title="Reset your page" @click="refreshPage">
     </div>
 </template>
 
 <style scoped>
+
+    div img {
+        height: 45px;
+        cursor: pointer;
+    }
+
     .form-select {
-        background-color: #D48F3B;
-        color: white;
-        border: none;
-        box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.2);
+        background: linear-gradient(135deg, #d48f3b, #b76b29);
+        width: 250px;
+        color: #fff;
+        border: 2px solid #000;
+        border-radius: 8px;
+        padding: 0.5rem;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        transition: all 0.3s ease;
+        font-weight: bold;
+        text-align: center;
+        cursor: pointer;
     }
 
     .form-select:focus {
         outline: none;
+        box-shadow: 0 0 8px 4px rgba(0, 0, 0, 0.3);
+        border-color: #ffb84d;
+    }
+
+    .form-select:hover {
+        background: linear-gradient(135deg, #e6a954, #c37b33);
+        color: #fff;
+    }
+
+    .form-select option {
+        color: #000;
+        background-color: #ffb84d;
     }
 </style>
